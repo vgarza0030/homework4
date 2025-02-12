60cbba88be4922fb18f1d45764793945b3909c48
Victor Garza <vgarza_0030@stu.southtexascollege.edu>
2025-02-11 20:24:17 -0600

    Finished addtransaction component

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import Header from './components/Header.vue';
 import Balance from './components/Balance.vue';
 import IncomeExpense from './components/IncomeExpense.vue';
+import AddTransaction from './components/AddTransaction.vue';
 import {ref, computed} from 'vue'
 const transactions = ref([
   {id: 1, test:'Paycheck', amount: 700.00},
@@ -43,6 +44,7 @@ const moneyOut = computed(()=>{
   <div class="container">
     <Balance :total="sum"></Balance>
     <IncomeExpense :income="moneyIn" :expense="moneyOut"></IncomeExpense>
+    <AddTransaction></AddTransaction>
   </div>
 
 
---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref, defineEmits } from 'vue';
+const text = ref('')
+const amount = ref('')
+
+const emit =defineEmits([
+    'transactionSubmitted'
+])
+
+const onSubmit = ()=>{
+    const transactionData = {
+        text: text.value,
+        amount: parseFloat(amount.value),
+    }
+
+    emit('transactionSubmitted', transactionData)
+
+    text.value = ''
+    amount.value = ''
+
+}
+
+</script>
+
+
+<template>
+    <h3>Add a New transactions</h3>
+    <form id="form" @submit.prevent="onSubmit">
+        <div class="form-control">
+            <label for="text">Enter Transaction</label>
+            <input type="text" id="text" v-model="text" placeholder="Enter Transaction...">
+        </div>
+        <div class="form-control">
+            <label for="amount">Enter Transaction Amount</label>
+            <input type="text" id="amount" v-model="amount" placeholder="Enter Negative Value for Expenses...">
+        </div>
+        <button class="btn">Add Transaction</button>
+
+    </form>
+
+
+</template>
